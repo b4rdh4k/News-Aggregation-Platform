de40e22c43b57994702a155eb4569a8ad6fa7d4f
Bardha Kryeziu <bk68313@ubt-uni.net>
2024-07-07 21:16:12 +0200

    Signup-Signin design fixes, responsivity needs a bit of work

--- client/src/components/Account/SigninForm.vue
+++ client/src/components/Account/SigninForm.vue
@@ -1,56 +1,57 @@
 <template>
-  <div class="flex min-h-screen bg-background text-text">
-    <!--have to change this illustration to something else that fits-->
-    <div v-if="shouldShowIllustration" class="flex flex-1 items-center justify-center">
-      <img src="@/assets/Signin-illustration.png" alt="Sign In Illustration" class="w-1/2" />
-    </div>
-    <div :class="formContainerClass">
-      <h1 class="text-4xl font-bold mb-4">Sign in</h1>
-      <button
-        class="w-full max-w-sm py-2 px-4 mb-4 border border-gray-300 rounded flex items-center justify-center hover:bg-gray-100"
-      >
-        <img
-          src="https://developers.google.com/identity/images/g-logo.png"
-          alt="Google Logo"
-          class="w-6 h-6 mr-2"
-        />
-        Sign in with Google
-      </button>
-      <div class="text-text mb-4">or use e-mail instead</div>
-      <form @submit.prevent="submitForm" class="w-full max-w-sm">
-        <div class="mb-4">
-          <label for="email" class="block mb-2">E-mail</label>
-          <input
-            type="email"
-            id="email"
-            v-model="email"
-            class="w-full p-2 border border-gray-300 rounded"
-          />
-        </div>
-        <div class="mb-4">
-          <label for="password" class="block mb-2">Password</label>
-          <input
-            type="password"
-            id="password"
-            v-model="password"
-            class="w-full p-2 border border-gray-300 rounded"
-          />
-        </div>
-        <button
-          type="submit"
-          class="w-full py-2 px-4 bg-dark-accent text-white rounded hover:bg-accent"
-        >
-          Sign in
-        </button>
-      </form>
-      <div class="mt-4">
-        <a href="#" class="text-secondary">Forgot password?</a>
+  <div class="flex min-h-screen bg-background text-text justify-center">
+    <div class="flex flex-1 items-start justify-center p-4">
+      <div v-if="shouldShowIllustration" class="flex flex-1 items-start justify-center p-4">
+        <img src="@/assets/Signin-svg.svg" alt="Sign In Illustration" class="svg-illustration w-full max-w-md" />
       </div>
-      <div class="mt-2">
-        <p class="text-secondary">
-          Not using Sapientia yet?
-          <span @click="goToSignUp" class="text-primary cursor-pointer">Sign up</span>
-        </p>
+      <div class="w-full max-w-sm pt-2">
+        <h1 class="text-4xl font-bold mb-4 text-center">Sign in</h1>
+        <button
+          class="w-full py-2 px-4 mb-4 border border-gray-300 rounded flex items-center justify-center text-dark-background bg-white hover:bg-gray-100"
+        >
+          <img
+            src="https://developers.google.com/identity/images/g-logo.png"
+            alt="Google Logo"
+            class="w-6 h-6 mr-2"
+          />
+          Sign up with Google
+        </button>
+        <div class="text-text mb-4 text-center">or use e-mail instead</div>
+        <form @submit.prevent="submitForm" class="w-full">
+          <div class="mb-4">
+            <label for="email" class="block mb-2">E-mail</label>
+            <input
+              type="email"
+              id="email"
+              v-model="email"
+              class="w-full p-2 border border-gray-300 rounded"
+            />
+          </div>
+          <div class="mb-4">
+            <label for="password" class="block mb-2">Password</label>
+            <input
+              type="password"
+              id="password"
+              v-model="password"
+              class="w-full p-2 border border-gray-300 rounded"
+            />
+          </div>
+          <button
+            type="submit"
+            class="w-full py-2 px-4 bg-dark-accent text-white rounded hover:bg-accent"
+          >
+            Sign in
+          </button>
+        </form>
+        <div class="mt-4 text-center">
+          <a href="#" class="text-secondary">Forgot password?</a>
+        </div>
+        <div class="mt-2 text-center">
+          <p class="text-secondary">
+            Not using Sapientia yet?
+            <span @click="goToSignUp" class="text-primary cursor-pointer">Sign up</span>
+          </p>
+        </div>
       </div>
     </div>
   </div>
@@ -65,23 +66,16 @@ export default {
       shouldShowIllustration: true
     }
   },
-  computed: {
-    formContainerClass() {
-      return this.shouldShowIllustration
-        ? 'flex flex-1 flex-col items-center '
-        : 'flex flex-1 flex-col items-center p-4'
-    }
-  },
   methods: {
     submitForm() {
       alert('Form submitted')
-      //etcetc
+      // form submission logic
     },
     goToSignUp() {
       this.$router.push('/sign-up')
     },
     updateIllustrationVisibility() {
-      this.shouldShowIllustration = window.innerWidth > 640
+      this.shouldShowIllustration = window.innerWidth > 1020
     }
   },
   mounted() {
@@ -103,5 +97,8 @@ input:focus {
 button:focus {
   outline: none;
   box-shadow: 0 0 0 3px rgba(255, 193, 7, 0.3);
+}
+.dark .svg-illustration {
+  filter: invert(93%) sepia(4%) saturate(10%) hue-rotate(5deg) brightness(99%) contrast(87%);
 }
 </style>
