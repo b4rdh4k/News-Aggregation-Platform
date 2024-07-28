1b9971473967d7f3c8dcde962cdac24a7677dd99
Bardha Kryeziu <bk68313@ubt-uni.net>
2024-07-28 07:05:25 +0200

    Lottie Vue library

--- client/src/admin/AdminViews/AdminHome.vue
+++ client/src/admin/AdminViews/AdminHome.vue
@@ -1,29 +1,19 @@
 <script setup>
-import { ref, onMounted } from 'vue'
-import lottie from 'lottie-web'
-import animationData from '@/assets/media/AdminAnimation.json'
-const lottieContainer = ref(null)
-onMounted(() => {
-  lottie.loadAnimation({
-    container: lottieContainer.value,
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    animationData
-  })
-})
+import { DotLottieVue } from '@lottiefiles/dotlottie-vue'
 </script>
 <template>
   <div>
-    <div
-      class="welcome-section flex items-center justify-between "
-    >
+    <div class="welcome-section">
       <div>
         <h3 class="text-2xl font-semibold">Welcome back Emri</h3>
         <p>Here's what's happening with our platform today</p>
       </div>
       <div class="welcome-animation">
-        <div ref="lottieContainer" class="h-full"></div>
+        <DotLottieVue
+          autoplay
+          loop
+          src="https://lottie.host/3ee910b1-e9cb-4c8d-aaec-0200067256ef/FcxaWo9YEF.lottie"
+        />
       </div>
     </div>
     <div class="card-grid">
