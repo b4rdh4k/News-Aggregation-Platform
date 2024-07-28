b3e84653469c201ab59faeb318c90a9fad8a88d0
Bardha Kryeziu <bk68313@ubt-uni.net>
2024-07-28 16:49:27 +0200

    Succes Payment and Failure pages design

--- client/src/views/Subscription/CancelPayment.vue
+++ client/src/views/Subscription/CancelPayment.vue
@@ -1,7 +1,36 @@
+<script setup>
+import { DotLottieVue } from '@lottiefiles/dotlottie-vue'
+</script>
+
 <template>
+  <div class="min-h-screen px-2 flex flex-col items-center text-center">
     <div>
-        <h1>
-        This is the subscription failure page
-        </h1>
+      <DotLottieVue
+        style="width: 350px; height: 300px"
+        autoplay
+        loop
+        src="https://lottie.host/05a31154-e571-4eb4-aa95-04040c6c1879/SnQnmEa0Jj.lottie"
+      />
     </div>
+    <h4 class="text-accent dark:text-dark-secondary">Payment Cancelled</h4>
+    <h5 class="text-secondary dark:text-dark-accent">Your payment was not successful.</h5>
+    <p class="p-2">If circumstances change, we'll welcome you with open arms</p>
+    <div class="flex flex-col md:flex-row gap-3 mt-2 mb-2">
+      <router-link to="/subscription">
+        <button
+          class="bg-accent dark:bg-dark-accent hover:bg-secondary dark:hover:bg-dark-secondary text-text dark:text-dark-text p-3 rounded-xl"
+        >
+          Try again
+        </button>
+      </router-link>
+      <router-link to="/">
+        <button
+          class="bg-primary dark:bg-dark-primary hover:bg-accent dark:hover:bg-dark-accent text-text dark:text-dark-text p-3 rounded-xl"
+        >
+          Back to Home
+        </button>
+      </router-link>
+    </div>
+  </div>
 </template>
+
--- client/src/views/Subscription/SuccessPayment.vue
+++ client/src/views/Subscription/SuccessPayment.vue
@@ -1,7 +1,26 @@
+<script setup>
+import { DotLottieVue } from '@lottiefiles/dotlottie-vue'
+</script>
+
 <template>
+  <div class="min-h-screen px-2 flex flex-col items-center text-center">
     <div>
-        <h1>
-        This is the subscription success page
-        </h1>
+      <DotLottieVue
+        style="width: 350px; height: 300px"
+        autoplay
+        loop
+        src="https://lottie.host/2ad01616-dd5a-412c-b1c4-43fa03841793/8dnrOmAzRR.lottie"
+      />
     </div>
+    <h4 class="text-accent dark:text-dark-secondary">Congratulations!</h4>
+    <h5 class="text-secondary dark:text-dark-accent">Your payment was successful!</h5>
+    <p class="mt-2 p-2">You're now officially the coolest Sapientian ever!</p>
+    <router-link to="/">
+      <button
+        class="bg-primary dark:bg-dark-primary hover:bg-accent dark:hover:bg-dark-accent text-text dark:text-dark-text p-3 rounded-xl mt-4"
+      >
+        Back to Home
+      </button>
+    </router-link>
+  </div>
 </template>
