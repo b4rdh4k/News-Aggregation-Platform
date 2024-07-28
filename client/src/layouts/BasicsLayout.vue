93df5a2e56a7855bc576046dab7fea5a5907470c
Shqiperim <ramadanishqiperim@gmail.com>
2024-07-28 12:29:18 +0200

    corrected docker file

--- client/src/layouts/BasicsLayout.vue
+++ client/src/layouts/BasicsLayout.vue
@@ -1,5 +1,5 @@
 <script setup>
-import ThemeToggle from "@/components/shared/Interactions/ThemeToggle.vue";
+import ThemeToggle from '@/components/shared/Interactions/ThemeToggle.vue'
 </script>
 
 <template>
@@ -7,7 +7,7 @@ import ThemeToggle from "@/components/shared/Interactions/ThemeToggle.vue";
     <header class="bg-background dark:bg-dark-background text-text dark:text-dark-text p-4 m-0">
       <div class="container mx-auto flex justify-between">
         <router-link to="/" class="h-12 flex-shrink-0">
-          <img src="@/assets/Sapientia-Logo.png" alt="Logo" class="h-full cursor-pointer" />
+          <img src="@/assets/media/Sapientia-Logo.png" alt="Logo" class="h-full cursor-pointer" />
         </router-link>
 
         <div class="flex items-center space-x-4">
