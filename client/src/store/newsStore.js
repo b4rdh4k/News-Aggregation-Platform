2e89a9256e106924eb9438b5f342db481a6e587e
MalaZogu <153452704+MalaZogu@users.noreply.github.com>
2024-08-01 23:31:37 +0200

    Card seperated from latest news, now it is reusable

---
+++ client/src/store/newsStore.js
@@ -0,0 +1,38 @@
+// src/stores/useNewsStore.js
+import { defineStore } from 'pinia'
+import { ref, computed } from 'vue'
+import { useToast } from 'vue-toastification'
+
+export const useNewsStore = defineStore('news', () => {
+  const toast = useToast()
+  const allNews = ref([])
+  const perPage = 12
+
+  const fetchAllStories = async () => {
+    try {
+      const response = await fetch('https://api.sapientia.life/article/all')
+      if (!response.ok) {
+        throw new Error('Network response was not ok')
+      }
+      const data = await response.json()
+      if (data.Value && Array.isArray(data.Value.Articles)) {
+        allNews.value = data.Value.Articles
+      } else {
+        console.error('Fetched data does not contain the expected array structure:', data.Value.Articles)
+      }
+    } catch (error) {
+      console.error('Error fetching all stories:', error)
+      toast.error('Failed to fetch stories.')
+    }
+  }
+
+  const visibleNews = computed(() => allNews.value.slice(0, perPage))
+  const hasNews = computed(() => visibleNews.value.length > 0)
+
+  return {
+    allNews,
+    visibleNews,
+    hasNews,
+    fetchAllStories
+  }
+})
